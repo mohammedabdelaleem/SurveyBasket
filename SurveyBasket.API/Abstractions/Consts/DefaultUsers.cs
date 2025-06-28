c815c359f90afe5b68cbef2b348b425b837b6fba
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-06-28 16:26:02 +0300

    partial class

--- SurveyBasket.API/Abstractions/Consts/DefaultUsers.cs
+++ SurveyBasket.API/Abstractions/Consts/DefaultUsers.cs
@@ -12,5 +12,16 @@ public static class DefaultUsers
 	public const string AdminPasswordHash = "AQAAAAIAAYagAAAAEOm19rPH9+gX4oopeWRXkyo+gPyFA3OBPZVkNori56HqKq38sCBNntaiBa7n8sADpA==";
 
 
+	//public partial class Admin
+	//{
+	//	public const string Id = "01978f6e-267c-7c96-b179-d2e08d73f69b";
+	//	public const string FirstName = "Survey Basket";
+	//	public const string LastName = "Admin";
+	//	public const string Email = "Admin@survey-basket.com";
+	//	public const string SecurityStamp = "35744942C16C49B9B2300A03094FAF85";
+	//	public const string ConcurrencyStamp = "01078f6e-267c-7c96-b179-d2e127b3bb2c";
+	//	//public const string Password = "Pass@123";
+	//	public const string PasswordHash = "AQAAAAIAAYagAAAAEOm19rPH9+gX4oopeWRXkyo+gPyFA3OBPZVkNori56HqKq38sCBNntaiBa7n8sADpA==";
+	//}
 
 }
