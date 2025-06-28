64c32cc9091e890921d46ef49b3c0584e5098928
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-06-28 16:24:01 +0300

    ues partial classes

--- SurveyBasket.API/Abstractions/Consts/DefaultRoles.cs
+++ SurveyBasket.API/Abstractions/Consts/DefaultRoles.cs
@@ -11,6 +11,20 @@ public static class DefaultRoles
 	public const string MemberRoleId = "01979187-c392-77be-89d1-ffe7d2df25d4";
 	public const string MemberConcurrencyStamp = "01979187-c392-77be-89d1-ffe8850dfea6";
 	
+	////we can do :
+	//public partial class Admin
+	//{
+	//	public const string Name = nameof(Admin);
+	//	public const string Id = "01979187-c392-77be-89d1-ffe5c2623fae";
+	//	public const string ConcurrencyStamp = "01979187-c392-77be-89d1-ffe6910b4102";
+	//}
 
+	//public partial class Member
+	//{
+	//	public const string Name = nameof(Member);
+	//	public const string Id = "01979187-c392-77be-89d1-ffe5c2623fae";
+	//	public const string ConcurrencyStamp = "01979187-c392-77be-89d1-ffe6910b4102";
+	//}
 
+	////then ===> DefaultRoles.Admin.Id , .....
 }
